1689d98eed2477f5b6f08f54f76411b5a62e5630
alzarus <alzarus.25@gmail.com>
2024-10-18 02:57:00 -0300

    fix: all services running

--- crawlers/run-crawlers.sh
+++ crawlers/run-crawlers.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Verifica se a API está acessível
-until $(curl --output /dev/null --silent --head --fail http://api:3000); do
-    echo "Aguardando API ficar disponível..."
-    sleep 5
-done
-
 # Executa cada crawler e redireciona o log para um arquivo separado
 npm run start-contract >> /var/log/contract.log 2>&1
 npm run start-councilor >> /var/log/councilor.log 2>&1
@@ -14,6 +8,3 @@ npm run start-general-productivity >> /var/log/general-productivity.log 2>&1
 npm run start-proposition >> /var/log/proposition.log 2>&1
 npm run start-proposition-productivity >> /var/log/proposition-productivity.log 2>&1
 npm run start-travel-expenses >> /var/log/travel-expenses.log 2>&1
-
-echo "Crawlers concluídos, iniciando json-processor..."
-docker-compose run json-processor
